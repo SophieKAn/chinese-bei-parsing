ffaf4cc677e645a27c596acfdc8a067f0504ee4a
ander569 <ander569@students.wwu.edu>
2016-05-28 15:19:40 -0700

    Process file line by line, putting each token and its tags into a list

--- processData.py
+++ processData.py
@@ -1,1 +1,26 @@
-#This is where I will write my program!
+## Sophia Anderson | ander569@wwu.edu | 28 March 2016
+
+import os
+import sys
+
+
+
+
+def main(args):
+
+  with open ("test.txt") as data_file:
+    line_number = 0;
+    for line in data_file:
+      line_number += 1
+      sentence = line.split("\t")
+      if len(sentence) > 1:
+        sentence[1] = sentence[1].decode("utf-8")
+      if u'\u88ab' in sentence:
+          print("It's a bei sentence!")
+
+
+  print(line_number)
+
+
+
+main(sys.argv)
