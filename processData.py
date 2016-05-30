9f81c038e859b51ccf422f0e391aa8f05c26d7a8
ander569 <ander569@students.wwu.edu>
2016-05-30 12:11:23 -0700

    instances of bei get separated into short and long

--- processData.py
+++ processData.py
@@ -7,11 +7,13 @@ import sys
 def main():
 
     bei_sentences = []
+    token_count = 0
 
-    with open ("test.txt") as data_file:
+    with open ("zh-ud-train.conllu") as data_file:
         is_bei = False;
         sentence = []
         for line in data_file:
+            token_count += 1
             token = line.split("\t")
             if len(token) > 1:
                 token[1] = token[1].decode("utf-8")
@@ -28,7 +30,35 @@ def main():
             else:
                 sentence.append(token)
 
+    print("{} total tokens.".format(token_count))
+    print("{} instances of the bei passive marker.".format(len(bei_sentences)))
 
+    long_passives = findLongs(bei_sentences)
+    short_passives = findShorts(bei_sentences)
+
+
+
+def findLongs(bei_sentences):
+    longs = []
+    for sentence in bei_sentences:
+        for token in range(len(sentence)):
+            if u'\u88ab' in sentence[token]:
+                if sentence[token+1][3] != 'VERB':
+                    longs.append(sentence)
+
+    print("{} instances of long passive sentences.".format(len(longs)))
+    return longs
+
+def findShorts(bei_sentences):
+    shorts = []
+    for sentence in bei_sentences:
+        for token in range(len(sentence)):
+            if u'\u88ab' in sentence[token]:
+                if sentence[token+1][3] == 'VERB':
+                    shorts.append(sentence)
+
+    print("{} instances of short passive sentences.".format(len(shorts)))
+    return shorts
 
 
 main()
