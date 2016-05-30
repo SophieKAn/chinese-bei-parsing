dad17788186609fe855f895a13fd6dce34c11aff
ander569 <ander569@students.wwu.edu>
2016-05-30 10:48:24 -0700

    processData can process sentence by sentence now

--- processData.py
+++ processData.py
@@ -4,23 +4,33 @@ import os
 import sys
 
 
-
-
 def main(args):
 
-  with open ("test.txt") as data_file:
-    line_number = 0;
-    for line in data_file:
-      line_number += 1
-      sentence = line.split("\t")
-      if len(sentence) > 1:
-        sentence[1] = sentence[1].decode("utf-8")
-      if u'\u88ab' in sentence:
-          print("It's a bei sentence!")
+    with open ("test.txt") as data_file:
+        line_number = 0;
+        sentence = [[]]
+        for line in data_file:
+            line_number += 1
+            token = line.split("\t")
+            if len(token) == 1:
+                sentence = [[]]
+            else:
+                sentence.append(token)
 
 
-  print(line_number)
 
+    for token in sentence:
+        if len(token) > 1:
+            print(token[1].decode("utf-8"))
+
+
+    ##if len(token) > 1:
+    ##  token[1] = token[1].decode("utf-8")
+    ##if u'\u88ab' in token:
+    ##    print("It's a bei sentence!")
+
+
+    ##print(line_number)
 
 
 main(sys.argv)
