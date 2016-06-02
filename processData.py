2e293f6390d50a1c1343ecb0c2b5ad603cd5ac7b
ander569 <ander569@students.wwu.edu>
2016-06-02 15:18:43 -0700

    pos/neg dictionaries are loaded into program

--- processData.py
+++ processData.py
@@ -8,6 +8,8 @@ def main():
 
     bei_sentences = []
     token_count = 0
+    bei_count = 0
+
 
     with open ("zh-ud-train.conllu") as data_file:
         is_bei = False;
@@ -19,6 +21,7 @@ def main():
                 token[1] = token[1].decode("utf-8")
             if u'\u88ab' in token:
                 is_bei = True
+                bei_count += 1
 
             if len(token) == 1 and is_bei == True:
                 bei_sentences.append(sentence)
@@ -31,10 +34,36 @@ def main():
                 sentence.append(token)
 
     print("{} total tokens.".format(token_count))
-    print("{} instances of the bei passive marker.".format(len(bei_sentences)))
+    print("{} instances of the bei passive marker.".format(bei_count))
 
     long_passives = findLongs(bei_sentences)
     short_passives = findShorts(bei_sentences)
+
+
+    print("Average distance from head for short sentences is {}".format(averageDistance(short_passives)))
+    print("Average distance from head for long sentences is {}".format(averageDistance(long_passives)))
+
+
+    ##Lets get these dictionaries!
+    positive = []
+    negative = []
+
+    with open("Sentiment/negative_comment.txt") as file:
+        for line in file:
+	    negative.append(line)
+
+    with open("Sentiment/negative_emotion.txt") as file:
+	for line in file:
+	    negative.append(line)
+
+    with open("Sentiment/positive_comment.txt") as file:
+	for line in file:
+	    positive.append(line)
+    with open("Sentiment/positive_emotion.txt") as file:
+	for line in file:
+	    positive.append(line)
+
+    print(positive.decode("utf-8"))
 
 
 
@@ -59,6 +88,18 @@ def findShorts(bei_sentences):
 
     print("{} instances of short passive sentences.".format(len(shorts)))
     return shorts
+
+
+def averageDistance(sentences):
+    summ = 0
+    count = 0
+
+    for sentence in sentences:
+        for token in sentence:
+            if u'\u88ab' in token:
+                count += 1
+                summ += int(token[6])
+    return summ/count
 
 
 main()
